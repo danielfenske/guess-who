9783360dfa50436229d8e67601c9545706f30a24
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-31 16:41:49 -0600

    got the loop to tell us if right or wrong

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ function readyNow(){
     console.log('JQ');
     $('header').append(`<h1>click on: ${people[dunker].name}</h1>`)
     displayDunkers();
+    $('')
 }
 
 function displayDunkers (){
@@ -26,4 +27,18 @@ function displayDunkers (){
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+function personFinder() {
+    for (let i=0;i<people.length;i++) {
+        if (i === dunker) {
+            alert(`${people[i].name}`)
+            return true;
+        } else {
+            alert('wrong, try again');
+            return false;
+        }
+    } // end of loop
+
+}// end personFinder
+
 
